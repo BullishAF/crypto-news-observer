0df01df246575d1f80f412a49cb9107ed768c49b
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2022-12-24 02:26:05 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Crypto News Observer';
+export const SITE_TITLE = 'On-Chain News';
 export const SITE_DESCRIPTION = 'The latest news in crypto, defi and nfts';
