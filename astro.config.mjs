bc7bef20f5e985c0f9a1f8283ed566f59f002df7
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2022-12-24 19:38:39 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.on-chain.news',
+  site: 'https://on-chain.news',
   integrations: [mdx(), sitemap(), tailwind()]
 });
